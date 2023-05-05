d10df4a1cac6a79abdb3475c3345094e8135dac1
Hutson_Huy <tanhuy.huc@gmail.com>
2023-05-05 17:34:55 +0700

    update dim_country

---
+++ models/staging/stg_dim_country.sql
@@ -0,0 +1,65 @@
+WITH dim_country__resource AS (
+  SELECT
+    *
+  FROM `vit-lam-data.wide_world_importers.application__countries`
+)
+
+, dim_country__rename_recast AS (
+  SELECT
+    CAST(country_id AS INT) AS country_key
+    , CAST(country_name AS STRING) AS country_name
+    , CAST(country_type AS STRING) AS country_type
+    , CAST(continent AS STRING) AS continent_name
+    , CAST(region AS STRING) AS region_name
+    , CAST(subregion AS STRING) AS subregion_name
+  FROM dim_country__resource
+)
+
+, dim_country__handle_null AS (
+  SELECT
+    country_key
+    , country_name
+    , COALESCE(country_type, 'Undefined') AS country_type
+    , COALESCE(continent_name, 'Undefined') AS continent_name
+    , COALESCE(region_name, 'Undefined') AS region_name
+    , COALESCE(subregion_name, 'Undefined') AS subregion_name
+  FROM dim_country__rename_recast
+)
+
+, dim_country__add_undefined AS (
+  SELECT
+    country_key
+    , country_name
+    , country_type
+    , continent_name
+    , region_name
+    , subregion_name
+  FROM dim_country__handle_null
+
+  UNION ALL 
+  SELECT
+    0 AS country_key
+    , 'Undefined' AS country_name
+    , 'Undefined' AS country_type
+    , 'Undefined' AS continent_name
+    , 'Undefined' AS region_name
+    , 'Undefined' AS subregion_name
+
+  UNION ALL 
+  SELECT
+    -1 AS country_key
+    , 'Invalid' AS country_name
+    , 'Invalid' AS country_type
+    , 'Invalid' AS continent_name
+    , 'Invalid' AS region_name
+    , 'Invalid' AS subregion_name
+)
+
+SELECT
+  country_key
+  , country_name
+  , country_type
+  , continent_name
+  , region_name
+  , subregion_name
+FROM dim_country__add_undefined
