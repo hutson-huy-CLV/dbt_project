5f880894528c762461b0f7f7af52d70fe458388b
Hutson_Huy <tanhuy.huc@gmail.com>
2023-05-05 16:19:30 +0700

    add dim_city

---
+++ models/analytics/dim_city.sql
@@ -0,0 +1,6 @@
+SELECT application__cities.city_name
+FROM `vit-lam-data.wide_world_importers.application__cities` application__cities
+LEFT JOIN `vit-lam-data.wide_world_importers.application__state_provinces`application__state_provinces
+ON application__cities.state_province_id = application__state_provinces.state_province_id
+LEFT JOIN `vit-lam-data.wide_world_importers.application__countries` application__countries
+ON application__countries.country_id = application__countries.country_id
--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -4,5 +4,5 @@ SELECT stock_item_id AS product_id
       , product.supplier_id
       , supplier.supplier_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`  AS product
-JOIN {{ref('supplier')}} AS supplier
+JOIN {{ref('dim_supplier')}} AS supplier
 ON product.supplier_id = supplier.supplier_id
