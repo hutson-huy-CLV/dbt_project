f0cf70d355d242ba9c200b30dfdcf2a61428307f
Hutson_Huy <tanhuy.huc@gmail.com>
2023-05-05 16:46:49 +0700

    modify dim_city

--- models/analytics/dim_city.sql
+++ models/analytics/dim_city.sql
@@ -1,6 +1,12 @@
-SELECT application__cities.city_name
+SELECT 
+  application__cities.city_id
+  ,COALESCE(application__cities.city_name, 'Invalid') AS city_name
+  ,COALESCE(application__state_provinces.state_province_name, 'Invalid') AS state_province_name
+  ,COALESCE(application__countries.continent, 'Invalid') AS continent
+  ,COALESCE(application__countries.region, 'Invalid') AS region
+  ,COALESCE(application__countries.subregion, 'Invalid') AS subregion
 FROM `vit-lam-data.wide_world_importers.application__cities` application__cities
-LEFT JOIN `vit-lam-data.wide_world_importers.application__state_provinces`application__state_provinces
+LEFT JOIN `vit-lam-data.wide_world_importers.application__state_provinces` application__state_provinces
 ON application__cities.state_province_id = application__state_provinces.state_province_id
 LEFT JOIN `vit-lam-data.wide_world_importers.application__countries` application__countries
 ON application__countries.country_id = application__countries.country_id
