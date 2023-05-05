a862b8773e49e9f615a559e2c30a2ad9d8aa3cfe
Hutson_Huy <tanhuy.huc@gmail.com>
2023-05-05 12:09:37 +0700

    Update product table

---
+++ models/analytics/product.sql
@@ -0,0 +1,8 @@
+SELECT stock_item_id AS product_id
+      , stock_item_name AS product_name
+      , COALESCE(brand, 'Undentified') AS brand_name
+      , product.supplier_id
+      , supplier.supplier_name
+FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`  AS product
+JOIN {{ref('supplier')}} AS supplier
+ON product.supplier_id = supplier.supplier_id
